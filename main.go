463eaa01b49da1b85dbeb708337ff9fd13e6abb3
ShaaradNJ <shaaradjarandikar@gmail.com>
2024-10-01 19:51:01 +0530

    update visiting website

--- main.go
+++ main.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-	"bufio"
+	_ "bufio"
 	"fmt"
-	"os"
-	"strings"
+	_ "os"
+	_ "strings"
 
 	"github.com/gocolly/colly/v2"
 )
 
 func main() {
-	var game_name string
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter a game name:")
-	game_name, _ = reader.ReadString('\n')
-	game_name_formatted := strings.ReplaceAll(game_name, " ", "+")
-	fmt.Println("The game name is:", game_name)
+	// var game_name string
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Println("Enter a game name:")
+	// game_name, _ = reader.ReadString('\n')
+	// game_name_formatted := strings.ReplaceAll(game_name, " ", "+")
+	// fmt.Println("The game name is:", game_name)
 	c := colly.NewCollector(colly.AllowedDomains(""))
 	c.OnHTML("h1", func(h *colly.HTMLElement) {
 		fmt.Println(h.Text)
 	})
-	defer c.Visit("https://technical.city/en/system-requirements")
-	fmt.Println(game_name_formatted)
-
+	defer c.Visit("https://www.systemrequirementslab.com/all-games-list")
+	// fmt.Println(game_name_formatted)
 }
