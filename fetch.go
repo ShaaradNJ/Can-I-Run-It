d97fde9623a6161047d219f610eecb9b58e9bf30
shaaradnj <shaaradjarandikar@gmail.com>
2024-12-31 10:47:16 +0530

    update scrape link

--- fetch.go
+++ fetch.go
@@ -24,7 +24,7 @@ func FetchGameRequirements(game_name string) (GameRequirements, error) {
 	var visit_here string
 	first_char := string(game_name[0])
 
-	domain := "https://www.systemrequirement"
+	domain := "https://www.systemrequirementslab.com/all-games-list"
 	c := colly.NewCollector()
 	c.SetClient(&http.Client{
 		Timeout: 30 * time.Second,
